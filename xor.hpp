4526c230808de7cc2d422d5758de77a4d296e05e
obseletecode72 <138137862+obseletecode72@users.noreply.github.com>
2024-10-16 08:28:33 -0700

    Update xor.hpp

--- xor.hpp
+++ xor.hpp
@@ -1,149 +1,224 @@
 #ifndef XOR_HPP
 #define XOR_HPP
 
-#include <cstddef>
-#include <cstdint>
-#include <cstring>
-#include <cstdlib>
+#include <immintrin.h>
+#include <stdint.h>
+#include <stddef.h>
+#include <intrin.h>
 
-template <typename T>
-struct remove_const {
-    typedef T type;
+template<typename T, T v>
+struct integral_constant {
+    static constexpr T value = v;
+    using value_type = T;
+    using type = integral_constant;
+    constexpr operator value_type() const noexcept { return value; }
+    constexpr value_type operator()() const noexcept { return value; }
 };
 
-template <typename T>
-struct remove_const<const T> {
-    typedef T type;
+template<typename T>
+struct remove_const { using type = T; };
+template<typename T>
+struct remove_const<const T> { using type = T; };
+template<typename T>
+using remove_const_t = typename remove_const<T>::type;
+
+template<typename T>
+struct remove_reference { using type = T; };
+template<typename T>
+struct remove_reference<T&> { using type = T; };
+template<typename T>
+struct remove_reference<T&&> { using type = T; };
+template<typename T>
+using remove_reference_t = typename remove_reference<T>::type;
+
+template<typename T, T... Ints>
+struct integer_sequence {
+    using value_type = T;
+    static constexpr size_t size() noexcept { return sizeof...(Ints); }
 };
 
-template <typename T>
-struct remove_reference {
-    typedef T type;
+template<size_t... Ints>
+using index_sequence = integer_sequence<size_t, Ints...>;
+
+template<size_t N, size_t... Ints>
+struct make_index_sequence_impl : make_index_sequence_impl<N - 1, N - 1, Ints...> {};
+
+template<size_t... Ints>
+struct make_index_sequence_impl<0, Ints...> {
+    using type = index_sequence<Ints...>;
 };
 
-template <typename T>
-struct remove_reference<T&> {
-    typedef T type;
-};
+template<size_t N>
+using make_index_sequence = typename make_index_sequence_impl<N>::type;
 
-template <typename T>
-struct remove_cv_ref {
-    typedef typename remove_const<typename remove_reference<T>::type>::type type;
-};
+template<typename T>
+struct make_unsigned;
 
-inline void clearBuffer(char* buffer, size_t size) {
-    volatile char* ptr = buffer;
-    while (size--) {
-        *ptr++ = 0;
+template<>
+struct make_unsigned<char> { using type = unsigned char; };
+
+template<>
+struct make_unsigned<short> { using type = unsigned short; };
+
+template<>
+struct make_unsigned<int> { using type = unsigned int; };
+
+template<>
+struct make_unsigned<long> { using type = unsigned long; };
+
+template<>
+struct make_unsigned<long long> { using type = unsigned long long; };
+
+template<>
+struct make_unsigned<unsigned char> { using type = unsigned char; };
+
+template<>
+struct make_unsigned<unsigned short> { using type = unsigned short; };
+
+template<>
+struct make_unsigned<unsigned int> { using type = unsigned int; };
+
+template<>
+struct make_unsigned<unsigned long> { using type = unsigned long; };
+
+template<>
+struct make_unsigned<unsigned long long> { using type = unsigned long long; };
+
+template<typename T>
+using make_unsigned_t = typename make_unsigned<T>::type;
+
+#define xorstr(str) ::jm::xor_string([]() { return str; }, integral_constant<size_t, sizeof(str) / sizeof(*str)>{}, make_index_sequence<::jm::detail::_buffer_size<sizeof(str)>()>{})
+#define xorstr_(str) xorstr(str).crypt_get()
+#define XORSTR_FORCEINLINE __forceinline
+
+namespace jm {
+    inline uint64_t ROTL64(uint64_t value, unsigned int count) noexcept {
+        return _rotl64(value, count);
     }
-}
 
-inline void clearBuffer(wchar_t* buffer, size_t size) {
-    volatile wchar_t* ptr = buffer;
-    while (size--) {
-        *ptr++ = 0;
-    }
-}
+    namespace detail {
+        constexpr uint64_t rotate_right_constexpr(uint64_t value, unsigned int count) noexcept {
+            return (value >> count) | (value << (64 - count));
+        }
 
-constexpr int convertTimeToSeconds(const char* time) {
-    return ((time[0] - '0') * 10 + (time[1] - '0')) * 3600 +
-        ((time[3] - '0') * 10 + (time[4] - '0')) * 60 +
-        ((time[6] - '0') * 10 + (time[7] - '0'));
-}
+        constexpr uint64_t rotate_left_constexpr(uint64_t value, unsigned int count) noexcept {
+            return (value << count) | (value >> (64 - count));
+        }
 
-constexpr int convertDateToNumber(const char* date) {
-    int month = ((date[0] - 'A') ^ (date[1] - 'A') ^ (date[2] - 'A')) & 0xFF;
-    int day = ((date[4] - '0') * 10 + (date[5] - '0')) & 0xFF;
-    int year = ((date[7] - '0') * 1000 + (date[8] - '0') * 100 +
-        (date[9] - '0') * 10 + (date[10] - '0')) & 0xFFFF;
-    return (month ^ day ^ year);
-}
+        inline uint64_t ROTR64(uint64_t value, unsigned int count) noexcept {
+            return _rotr64(value, count);
+        }
 
-constexpr unsigned char generateComplexSeed() {
-    int timeSeed = convertTimeToSeconds(__TIME__) % 256;
-    int dateSeed = convertDateToNumber(__DATE__) % 256;
-    unsigned char combinedSeed = static_cast<unsigned char>((timeSeed ^ dateSeed) & 0xFF);
-    combinedSeed = ((combinedSeed << 3) | (combinedSeed >> 5)) ^ 0xA5;
-    return combinedSeed;
-}
+        constexpr uint64_t apply_xor(uint64_t value, uint64_t key) noexcept {
+            return value ^ key;
+        }
 
-constexpr unsigned char XOR_SEED = generateComplexSeed();
+        constexpr uint64_t apply_not(uint64_t value) noexcept {
+            return ~value;
+        }
 
-constexpr uint64_t generateLargeSeed() {
-    uint64_t timeSeed = static_cast<uint64_t>(convertTimeToSeconds(__TIME__));
-    uint64_t dateSeed = static_cast<uint64_t>(convertDateToNumber(__DATE__));
-    uint64_t combinedSeed = (timeSeed << 32) | dateSeed;
-    combinedSeed = ((combinedSeed << 13) | (combinedSeed >> 19)) ^ (XOR_SEED ^ 100);
-    return combinedSeed;
-}
+        template<size_t Size>
+        XORSTR_FORCEINLINE constexpr size_t _buffer_size()
+        {
+            return ((Size / 16) + (Size % 16 != 0)) * 2;
+        }
 
-constexpr uint64_t LARGE_XOR_SEED = generateLargeSeed();
+        template<uint32_t Seed>
+        XORSTR_FORCEINLINE constexpr uint32_t key4() noexcept
+        {
+            uint32_t value = Seed;
+            for (char c : __TIME__)
+                value = static_cast<uint32_t>((value ^ c) * 16777619ull);
+            return value;
+        }
 
-constexpr unsigned char ROR_VALUE = (XOR_SEED % 4) + 1;
+        template<size_t N>
+        XORSTR_FORCEINLINE constexpr uint64_t key8()
+        {
+            constexpr auto first_part = key4<2166136261 + N>();
+            constexpr auto second_part = key4<first_part>();
+            return (static_cast<uint64_t>(first_part) << 32) | second_part;
+        }
 
-#pragma optimize("", off)
+        XORSTR_FORCEINLINE uint64_t load_from_reg(uint64_t value) noexcept
+        {
+            volatile uint64_t reg = value;
+            return reg;
+        }
 
-constexpr unsigned char applyXOR(uint64_t b, unsigned char c, unsigned char seed) {
-    return c ^ seed ^ b;
-}
+        template<size_t N, class CharT>
+        XORSTR_FORCEINLINE constexpr uint64_t load_rotated_xor_not_str8(uint64_t key, size_t idx, const CharT* str) noexcept
+        {
+            using cast_type = typename make_unsigned<CharT>::type;
+            constexpr auto value_size = sizeof(CharT);
+            constexpr auto idx_offset = 8 / value_size;
 
-#define APPLY_ROR(c, shift) (((c) >> (shift)) | ((c) << (8 - (shift))))
-#define APPLY_ROL(c, shift) (((c) << (shift)) | ((c) >> (8 - (shift))))
+            uint64_t value = 0;
+            for (size_t i = 0; i < idx_offset && i + idx * idx_offset < N; ++i) {
+                value |= (uint64_t{ static_cast<cast_type>(str[i + idx * idx_offset]) }
+                << ((i % idx_offset) * 8 * value_size));
+            }
 
-#define ENCRYPT_CHAR(b, c, seed) \
-    APPLY_ROR(applyXOR((b), (c), static_cast<unsigned char>(seed)), ROR_VALUE)
+            value = rotate_right_constexpr(value, (key & 0x3F));
+            value = apply_xor(value, key);
+            value = apply_not(value);
 
-template <typename CharT, size_t N>
-struct EncryptedStringT {
-    CharT encrypted_chars[N - 1];
-
-    constexpr EncryptedStringT(const CharT(&str)[N]) : encrypted_chars{} {
-        for (size_t i = 0; i < N - 1; ++i) {
-            encrypted_chars[i] = static_cast<CharT>(ENCRYPT_CHAR(LARGE_XOR_SEED, static_cast<unsigned char>(str[i]), XOR_SEED));
+            return value;
         }
     }
 
-    void decrypt(CharT* buffer) const {
-        for (size_t i = 0; i < N - 1; ++i) {
-            unsigned char decrypted_char = encrypted_chars[i];
-            decrypted_char = APPLY_ROL(decrypted_char, ROR_VALUE);
-            buffer[i] = static_cast<CharT>(applyXOR(LARGE_XOR_SEED, decrypted_char, XOR_SEED));
+    template<class CharT, size_t Size, class Keys, class Indices>
+    class xor_string;
+
+    template<class CharT, size_t Size, uint64_t... Keys, size_t... Indices>
+    class xor_string<CharT, Size, integer_sequence<uint64_t, Keys...>, index_sequence<Indices...>> {
+        constexpr static inline uint64_t alignment = ((Size > 16) ? 32 : 16);
+
+        alignas(alignment) uint64_t _storage[sizeof...(Keys)];
+
+    public:
+        using value_type = CharT;
+        using size_type = size_t;
+        using pointer = CharT*;
+        using const_pointer = const CharT*;
+
+        template<class L>
+        XORSTR_FORCEINLINE xor_string(L l, integral_constant<size_t, Size>, index_sequence<Indices...>) noexcept
+            : _storage{ ::jm::detail::load_from_reg((integral_constant<uint64_t, detail::load_rotated_xor_not_str8<Size>(Keys, Indices, l())>::value))... }
+        {}
+
+        XORSTR_FORCEINLINE constexpr size_type size() const noexcept
+        {
+            return Size - 1;
         }
-        buffer[N - 1] = static_cast<CharT>(0);
-    }
-};
 
-#pragma optimize("", on)
+        XORSTR_FORCEINLINE pointer crypt_get() noexcept
+        {
+            alignas(alignment) uint64_t keys[]{ ::jm::detail::load_from_reg(Keys)... };
 
-template <typename CharT, size_t N>
-class DecryptedBuffer {
-public:
-    CharT* buffer;
+            for (size_t i = 0; i < sizeof(_storage) / sizeof(uint64_t); ++i) {
+                _storage[i] = ::jm::detail::apply_not(_storage[i]);
+            }
 
-    DecryptedBuffer(const EncryptedStringT<CharT, N>& encrypted) {
-        buffer = static_cast<CharT*>(malloc(N * sizeof(CharT)));
-        if (buffer) {
-            encrypted.decrypt(buffer);
+            for (size_t i = 0; i < sizeof(_storage) / sizeof(uint64_t); ++i) {
+                _storage[i] ^= keys[i];
+            }
+
+            for (size_t i = 0; i < sizeof(_storage) / sizeof(uint64_t); ++i) {
+                _storage[i] = ROTL64(_storage[i], (keys[i] & 0x3F));
+            }
+
+            return reinterpret_cast<pointer>(_storage);
         }
-    }
+    };
 
-    ~DecryptedBuffer() {
-        if (buffer) {
-            clearBuffer(buffer, N);
-            free(buffer);
-        }
-    }
+    template<class L, size_t Size, size_t... Indices>
+    xor_string(L l, integral_constant<size_t, Size>, index_sequence<Indices...>) -> xor_string<
+        remove_const_t<remove_reference_t<decltype(l()[0])>>,
+        Size,
+        integer_sequence<uint64_t, detail::key8<Indices>()...>,
+        index_sequence<Indices...>>;
 
-    const CharT* get() const {
-        return buffer;
-    }
-};
-
-#define RXor(str) ([]() -> const auto* { \
-    using CharT = typename remove_cv_ref<decltype(str[0])>::type; \
-    static constexpr EncryptedStringT<CharT, sizeof(str)/sizeof(str[0])> encrypted{str}; \
-    static DecryptedBuffer<CharT, sizeof(str)/sizeof(str[0])> buffer{encrypted}; \
-    return buffer.get(); \
-}())
+}
 
 #endif
